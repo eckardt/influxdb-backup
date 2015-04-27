e4c88a15f977b5117f408dc4116adabe9b600c59
palkan <dementiev.vm@gmail.com>
2015-04-27 17:12:32 +0300

    Add series and start/end time options

--- influxdb-dump/main.go
+++ influxdb-dump/main.go
@@ -6,12 +6,16 @@ import (
   "io"
   "log"
   "os"
+  "strings"
   "github.com/eckardt/influxdb-go"
 )
 
 type ClientConfig struct {
   *influxdb.ClientConfig
   Destination string
+  Series string
+  StartTime string
+  EndTime string
 }
 
 type Client struct {
@@ -20,11 +24,14 @@ type Client struct {
 }
 
 func parseFlags() (*ClientConfig) {
-  config := &ClientConfig{&influxdb.ClientConfig{}, ""}
+  config := &ClientConfig{&influxdb.ClientConfig{}, "", "", "", ""}
   flag.StringVar(&config.Host, "host", "localhost:8086", "host to connect to")
   flag.StringVar(&config.Username, "username", "root", "username to authenticate as")
   flag.StringVar(&config.Password, "password", "root", "password to authenticate with")
   flag.StringVar(&config.Database, "database", "", "database to dump")
+  flag.StringVar(&config.Series, "series", "/.*/", "series name to dump")
+  flag.StringVar(&config.StartTime, "start", "", "time since series must be dumped")
+  flag.StringVar(&config.EndTime, "end", "", "time till series must be dumped")
   flag.StringVar(&config.Destination, "out", "-", "output file (default to stdout)")
   flag.BoolVar(&config.IsSecure, "https", false, "connect via https")
   flag.Parse()
@@ -49,6 +56,11 @@ func main() {
 func (self *Client) DumpSeries() {
   var err error
   var file io.Writer
+  var query string
+  var where_time string
+
+  times := make([]string, 0)
+
   if self.Destination != "-" {
     file, err = os.Create(self.Destination)
     if err != nil {
@@ -57,7 +69,26 @@ func (self *Client) DumpSeries() {
   } else {
     file = os.Stdout
   }
-  err = self.QueryStream("SELECT * FROM /.*/", file)
+
+  if self.StartTime != "" {
+    times = append(times, fmt.Sprintf("time > %s", self.StartTime))
+  } 
+
+  if self.EndTime != "" {
+    times = append(times, fmt.Sprintf("time < %s", self.EndTime))
+  }
+
+  if len(times) > 0 {
+    where_time = fmt.Sprintf("where %s", strings.Join(times, " and "))
+  }else{
+    where_time = ""
+  }
+
+  query = fmt.Sprintf("SELECT * FROM %s %s", self.Series, where_time)
+
+  log.Println("Query:", query)
+
+  err = self.QueryStream(query, file)
   if err != nil {
     log.Fatal(err)
   }
